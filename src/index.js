037c72864e773e40a58c7500f6e8e545af42602d
Taylor Kennedy <taykenned@gmail.com>
2016-07-11 09:47:44 -1000

    creates hapi server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log('Hello World');
+'use strict';
+
+const Hapi = require('hapi');
+const PORT = process.env.PORT || 3000
+
+const server = new Hapi.Server();
+server.connection({port: PORT});
+
+let counterStore = { counter: 0 };
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: function (request, reply) {
+    reply(counterStore);
+  }
+});
+
+server.start((err) => {
+  if(err) {
+    throw err;
+  }
+
+  console.log('Server running at:', server.info.uri);
+});
