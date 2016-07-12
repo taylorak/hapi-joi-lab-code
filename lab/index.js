c670e6381e111297170caf5cff5619fb257278c6
Taylor Kennedy <taykenned@gmail.com>
2016-07-11 15:18:21 -1000

    adds tests for counter api

--- lab/index.js
+++ lab/index.js
@@ -3,8 +3,8 @@
 const Code = require('code');
 const Lab = require('lab');
 const lab = exports.lab = Lab.script();
-const server = require("../src/");
-
+const server = require('../src/');
+let counterStore = require('../src/counterStore');
 const expect = Code.expect;
 
 lab.experiment('GET /', () => {
@@ -31,6 +31,7 @@ lab.experiment('GET /counter', () => {
       expect(result).to.be.an.object();
       expect(result).to.contain('counter');
       expect(result.counter).to.be.a.number();
+      expect(result.counter).to.equal(0);
       done();
     });
   });
@@ -40,11 +41,63 @@ lab.experiment('GET /counter', () => {
 lab.experiment('POST /counter', () => {
 
   lab.test('sets the counter value', (done) => {
-    done();
+    let counter = 50;
+    server.inject({
+      method: "POST",
+      url: "/counter",
+      payload: {
+        counter: counter
+      }
+    }, function(response) {
+      let result = response.result;
+      expect(response.statusCode).to.equal(200);
+      expect(result).to.contain('counter');
+      expect(result.counter).to.be.a.number();
+      expect(result.counter).to.equal(counter);
+      done();
+    });
   });
 
-  lab.test('trigger an error if counter > 1000 or counter < 0', (done) => {
-    done();
+  lab.test('trigger an error if counter > 1000', (done) => {
+    let counter = 1002;
+    server.inject({
+      method: "POST",
+      url: "/counter",
+      payload: {
+        counter: counter
+      }
+    }, function(response) {
+      expect(response.statusCode).to.equal(400);
+      done();
+    });
+  });
+
+  lab.test('trigger an error if counter < 0', (done) => {
+    let counter = -2;
+    server.inject({
+      method: "POST",
+      url: "/counter",
+      payload: {
+        counter: counter
+      }
+    }, function(response) {
+      expect(response.statusCode).to.equal(400);
+      done();
+    });
+  });
+
+  lab.test('trigger an error if counter is not a number', (done) => {
+    let counter = 'zero';
+    server.inject({
+      method: "POST",
+      url: "/counter",
+      payload: {
+        counter: counter
+      }
+    }, function(response) {
+      expect(response.statusCode).to.equal(400);
+      done();
+    });
   });
 
 });
@@ -52,13 +105,60 @@ lab.experiment('POST /counter', () => {
 lab.experiment('PUT /counter/increment', () => {
 
   lab.test('increment the counter value', (done) => {
-
+    counterStore.counter = 0;
+    let initialCounter = counterStore.counter;
+    server.inject({
+      method: "PUT",
+      url: "/counter/increment",
+    }, function(response) {
+      let result = response.result;
+      expect(response.statusCode).to.equal(200);
+      expect(result).to.be.an.object();
+      expect(result).to.contain('counter');
+      expect(result.counter).to.be.a.number();
+      expect(result.counter).to.equal(initialCounter + 1);
+      done();
+    });
   });
 
   lab.test('trigger an error if counter > 1000', (done) => {
-  })
+    counterStore.counter = 1000;
+    server.inject({
+      method: "PUT",
+      url: "/counter/increment",
+    }, function(response) {
+      expect(response.statusCode).to.equal(400);
+      done();
+    });
+  });
 });
 
 lab.experiment('PUT /counter/decrement', () => {
+  lab.test('decrement the counter value', (done) => {
+    counterStore.counter = 1000;
+    let initialCounter = counterStore.counter;
+    server.inject({
+      method: "PUT",
+      url: "/counter/decrement",
+    }, function(response) {
+      let result = response.result;
+      expect(response.statusCode).to.equal(200);
+      expect(result).to.be.an.object();
+      expect(result).to.contain('counter');
+      expect(result.counter).to.be.a.number();
+      expect(result.counter).to.equal(initialCounter - 1);
+      done();
+    });
+  });
 
+  lab.test('trigger an error if counter < 0', (done) => {
+    counterStore.counter = 0;
+    server.inject({
+      method: "PUT",
+      url: "/counter/decrement",
+    }, function(response) {
+      expect(response.statusCode).to.equal(400);
+      done();
+    });
+  });
 });
